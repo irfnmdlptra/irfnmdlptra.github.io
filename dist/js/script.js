202c8dae7b0f81123fe3684ee1e1c13983a78129
Irfnmdlptra <bangasrat@gmail.com>
2022-12-24 18:54:05 +0800

    menambahkan sesuatu

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,10 +19,12 @@ window.onscroll = function() {
         header.classList.add('navbar-fixed');
         toTop.classList.remove('hidden');
         toTop.classList.add('flex');
+     
     } else {
         header.classList.remove('navbar-fixed');
         toTop.classList.remove('flex');
         toTop.classList.add('hidden');
+       
     }
 }
 
