664ff53c04bfb30f64e51f276bb33dd8713e512f
Irfnmdlptra <bangasrat@gmail.com>
2022-12-29 10:18:31 +0800

    menambahkan animation loading

--- dist/js/script.js
+++ dist/js/script.js
@@ -59,3 +59,10 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
     darkToggle.checked =false;
     
 }
+
+
+var load = document.getElementById("load");
+
+window.addEventListener('load', function(){
+  load.style.display = 'none';
+})
